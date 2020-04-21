0aa34d5bb1afddc7ff7aa7ffb8fc5e92d6e2b216
kaiorr <kaio.rocharibeiro@gmail.com>
2020-04-21 16:13:05 -0300

    feat: iniciado nova função de captar operações

--- src/controllers/CalcController.js
+++ src/controllers/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
   constructor() {
 
+    this._operation = []
     this._locale = 'pt-BR'
     this._displayCalcEl = document.querySelector("#display")
     this._dateEl = document.querySelector("#data")
@@ -24,6 +25,86 @@ class CalcController {
     },1000)
 
   }
+
+  clearAll() {
+    this._operation = []
+  }
+
+  getLastOperation() {
+    return this._operation[this._operation.length-1]
+  }
+
+  clearEntry() {
+    this._operation.pop()
+  }
+
+  addOperation(value) {
+    this._operation.push(value)
+  }
+
+  setError() {
+    this.displayCalc = "Error"
+  }
+
+    execBtn(value){
+
+      switch(value) {
+        case 'ac':
+          this.clearAll()
+        break
+
+        case 'ce':
+          this.clearEntry()
+        break
+
+        case 'soma':
+          this.clearEntry()
+        break
+
+        case 'subtracao':
+          this.clearEntry()
+        break
+
+        case 'divisao':
+          this.clearEntry()
+        break
+
+        case 'multiplicacao':
+          this.clearEntry()
+        break
+
+        case 'porcento':
+          this.clearEntry()
+        break
+
+        case 'igual':
+          this.clearEntry()
+        break
+
+        case 'ponto':
+          this.clearEntry()
+        break
+
+        case '0':
+          case '1':
+            case '2':
+              case '3':
+                case '4':
+                  case '5':
+                    case '6':
+                      case '7':
+                        case '8':
+                          case '9':
+                            this.addOperation(parseInt(value))
+                              break
+        
+        default:
+          this.setError()
+        break
+      }
+       
+    }
+
   //desestruturando o método de evento, para considerar a virgula como separador, ficando assim
   //um array para percorrer com forEach
   addEventListenerAll(element, events, fn){
