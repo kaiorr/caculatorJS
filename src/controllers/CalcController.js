3d442ce4350439adf274be6f51fc9698e981e3e9
kaiorr <kaio.rocharibeiro@gmail.com>
2020-04-21 08:02:26 -0300

    feat: adicionado addEventListenerAll nos botoes para captura e pointer

--- src/controllers/CalcController.js
+++ src/controllers/CalcController.js
@@ -8,6 +8,7 @@ class CalcController {
     this._timeEl = document.querySelector("#hora")
     this._currentDate
     this.initialize()
+    this.initButtonsEvents()
 
   }
 
@@ -21,6 +22,39 @@ class CalcController {
       this.setDisplayDateTime()
 
     },1000)
+
+  }
+  //desestruturando o método de evento, para considerar a virgula como separador, ficando assim
+  //um array para percorrer com forEach
+  addEventListenerAll(element, events, fn){
+
+    events.split(',').forEach(event => {
+
+      element.addEventListener(event, fn, false)
+
+    })
+
+  }
+  
+  initButtonsEvents(){
+
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g")
+
+    buttons.forEach((btn, index)=>{
+
+      this.addEventListenerAll(btn, "click, drag", e => {
+      
+        console.log(btn.className.baseVal.replace("btn-", ""))
+
+      })
+
+      this.addEventListenerAll(btn, "mouseover, mouseup, mousedown", e => {
+
+        btn.style.cursor = "pointer"
+
+      })
+
+    })
 
   }
 
