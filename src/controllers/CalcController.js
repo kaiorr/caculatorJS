36d361758568250af365b9dc84c072f13157490a
kaiorr <kaio.rocharibeiro@gmail.com>
2020-04-23 07:07:02 -0300

    feat: add numeros concatenados e também <>  de numero com operator

--- src/controllers/CalcController.js
+++ src/controllers/CalcController.js
@@ -27,94 +27,162 @@ class CalcController {
   }
 
   clearAll() {
-    this._operation = []
+   this._operation = []
   }
 
-  getLastOperation() {
-    return this._operation[this._operation.length-1]
-  }
-
+  //remover última posição do array
   clearEntry() {
     this._operation.pop()
   }
 
+  //pegar última posição do array
+  getLastOperation() {
+    return this._operation[this._operation.length-1]
+  }
+
+  setLastOperation(value){
+    this._operation[this._operation.length - 1] = value
+  }
+
+  //busca dentro do array se existe algum dos elementos, senão encontrar vai retornar -1
+  isOperator(value) {
+    return (['+', '-', '*', '%', '/'].indexOf(value) > -1)
+  }
+
+  pushOperation(value) {
+     this._operation.push(value)
+
+     if (this._operation.length > 3) { 
+
+      this.calc()
+
+     }
+
+  }
+
+  calc(){
+
+    let last = this._operation.pop()
+
+    let result = eval(this._operation.join(""))
+
+    this._operation = [result, last]
+
+  }
+
+  //verificar se é número e se for converte o número para string, para se tornar possível
+  //concatenar o mesmo
   addOperation(value) {
-    this._operation.push(value)
+
+    
+
+    if (isNaN(this.getLastOperation())) {
+
+      if (this.isOperator(value)) {
+
+        this.setLastOperation(value)
+
+      } else if (isNaN(value)) {
+
+        console.log(value)
+
+      } else {
+
+        this._operation.push(value)
+
+      }
+
+    } else {
+
+      if (this.isOperator(value)) {
+
+        this._operation.push(value)
+
+      } else {
+
+        let newValue = this.getLastOperation().toString() + value.toString()
+
+        this.setLastOperation(parseInt(newValue))
+
+      }
+
+    } 
+
   }
 
   setError() {
     this.displayCalc = "Error"
   }
 
-    execBtn(value){
-
-      switch(value) {
-        case 'ac':
-          this.clearAll()
-        break
-
-        case 'ce':
-          this.clearEntry()
-        break
-
-        case 'soma':
-          this.clearEntry()
-        break
-
-        case 'subtracao':
-          this.clearEntry()
-        break
-
-        case 'divisao':
-          this.clearEntry()
-        break
-
-        case 'multiplicacao':
-          this.clearEntry()
-        break
-
-        case 'porcento':
-          this.clearEntry()
-        break
-
-        case 'igual':
-          this.clearEntry()
-        break
-
-        case 'ponto':
-          this.clearEntry()
-        break
-
-        case '0':
-          case '1':
-            case '2':
-              case '3':
-                case '4':
-                  case '5':
-                    case '6':
-                      case '7':
-                        case '8':
-                          case '9':
-                            this.addOperation(parseInt(value))
-                              break
-        
-        default:
-          this.setError()
-        break
-      }
-       
-    }
-
   //desestruturando o método de evento, para considerar a virgula como separador, ficando assim
   //um array para percorrer com forEach
   addEventListenerAll(element, events, fn){
 
-    events.split(',').forEach(event => {
+    events.split(' ').forEach(event => {
 
       element.addEventListener(event, fn, false)
 
     })
 
+  }
+
+  execBtn(value){
+
+    switch(value) {
+      case 'ac':
+        this.clearAll()
+      break;
+
+      case 'ce':
+        this.clearEntry()
+      break;
+
+      case 'soma':
+        this.addOperation('+')
+      break;
+
+      case 'subtracao':
+        this.addOperation('-')
+      break;
+
+      case 'divisao':
+        this.addOperation('/')
+      break;
+
+      case 'multiplicacao':
+        this.addOperation('*')
+      break;
+
+      case 'porcento':
+        this.addOperation('%')
+      break;
+
+      case 'igual':
+        
+      break;
+
+      case 'ponto':
+        this.addOperation('.')
+      break;
+
+      case '0':
+        case '1':
+          case '2':
+            case '3':
+              case '4':
+                case '5':
+                  case '6':
+                    case '7':
+                      case '8':
+                        case '9':
+                          this.addOperation(parseInt(value))
+                            break;
+      
+      default:
+        this.setError()
+      break;
+    }
+     
   }
   
   initButtonsEvents(){
@@ -123,13 +191,15 @@ class CalcController {
 
     buttons.forEach((btn, index)=>{
 
-      this.addEventListenerAll(btn, "click, drag", e => {
+      this.addEventListenerAll(btn, "click drag", e => {
       
-        console.log(btn.className.baseVal.replace("btn-", ""))
+       let textBtn =  btn.className.baseVal.replace("btn-", "")
+
+       this.execBtn(textBtn)
 
       })
 
-      this.addEventListenerAll(btn, "mouseover, mouseup, mousedown", e => {
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
 
         btn.style.cursor = "pointer"
 
